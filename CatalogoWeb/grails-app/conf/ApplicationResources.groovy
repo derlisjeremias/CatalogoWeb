49ec3f1bf92ed84a15c8ec65ea69763196d8846e
matiasgel <matiasgel@hotmail.com>
2013-06-15 01:16:55 -0300

    cambio de prueba

--- CatalogoWeb/grails-app/conf/ApplicationResources.groovy
+++ CatalogoWeb/grails-app/conf/ApplicationResources.groovy
@@ -1,8 +1,8 @@
 modules = {
     application {
         resource url:'js/application.js'
-        resource url:'vendor/modernizr-2.6.2-respond-1.1.0.min.js'
+        resource url:'js/vendor/modernizr-2.6.2-respond-1.1.0.min.js'
         resource url:'js/vendor/bootstrap.min.js'
-        resource url:'js/main'
+        resource url:'js/main.js'
     }
 }
